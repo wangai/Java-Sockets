1d5d439c556a175206c174391c8767c3b8379b28
Yours Truly <smmwangai@gmail.com>
2018-07-02 01:25:36 +0300

    added client and server

---
+++ Client.java
@@ -0,0 +1,70 @@
+/*********************
+* Socket Client
+* @file Client.java
+* @author Wangai
+**********************/
+
+import java.net.*;
+import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Client {
+
+   public static void main(String [] args) {
+      Socket client = null;
+      String host = args[0];
+      int port = Integer.parseInt(args[1]);
+      try {
+         client = new Socket(host, port);
+
+         // Set up streams for server I/O
+         InputStream inFromServer = client.getInputStream();
+         DataInputStream input = new DataInputStream(inFromServer);
+
+         OutputStream outToServer = client.getOutputStream();
+         DataOutputStream output = new DataOutputStream(outToServer);
+         
+         //Set up stream for keyboard entry
+         Scanner userEntry = new Scanner(System.in);
+         
+         // System.out.println("Server says " + input.readUTF());
+         System.out.println(input.readUTF());
+         
+         int service, input1, input2;
+         do {
+            System.out.print("\nservice: ");
+            service = userEntry.nextInt();
+            System.out.print("input1 : ");
+            input1 = userEntry.nextInt();
+            System.out.print("input2 : ");
+            input2 = userEntry.nextInt();
+
+            //send the numbers
+            output.writeInt(service);
+            output.writeInt(input1);
+            output.writeInt(input2);
+
+            // System.out.println("\nSERVER> " + input.readUTF());
+            
+            System.out.println(input.readUTF());
+
+         } while (service != 0);
+
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      catch (NoSuchElementException ne){   //This exception may be raised when the server closes connection
+         System.out.println("Connection closed");
+      }
+      finally {
+         try {
+            System.out.println("\n* Closing connection *");
+            client.close();
+         } catch (IOException ioEx) {
+            System.out.println("Unable to disconnect");
+            System.exit(1);
+         }
+      }
+   }
+}
---
+++ Server.java
@@ -0,0 +1,134 @@
+/*********************
+* Socket Server
+* @file Server.java
+* @author Wangai
+**********************/
+
+import java.net.*;
+import java.io.*;
+
+public class Server extends Thread {
+   private ServerSocket serverSocket;
+   
+   public Server(int port) throws IOException {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setSoTimeout(1000000);
+   }
+
+   public static void main(String [] args) {
+      int port = Integer.parseInt(args[0]);
+      try {
+         // ? do this in a seperate process?
+         Thread t = new Server(port);
+         t.start();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void run() {
+
+      while(true) {
+
+         try {
+
+            System.out.println("Waiting for client on port " + 
+               serverSocket.getLocalPort());
+
+            Socket server = serverSocket.accept();
+
+            System.out.println("Just connected to " + server.getRemoteSocketAddress());
+
+            DataInputStream input = new DataInputStream(server.getInputStream());
+            DataOutputStream output = new DataOutputStream(server.getOutputStream());
+            
+            // initial interaction ** send menu
+            output.writeUTF("\n\nSERVER>\n" + menu());
+            
+            int service = input.readInt();
+            int input1 = input.readInt();
+            int input2 = input.readInt();
+
+            /*
+            System.out.println("service: " + service);
+            System.out.println("input1: " + input1);
+            System.out.println("input2: " + input2);
+            */
+
+            do {
+               
+               // ** evaluate request
+               if (service == 1) {
+
+                  output.writeUTF("\n\nSERVER>\n" + menu());
+
+               } else if (service == 2) {
+
+                  output.writeUTF("SERVER>\n\t " + add(input1, input2));
+
+               } else if (service == 3) {
+
+                  output.writeUTF("SERVER>\n\t " + diff(input1, input2));
+                  
+               } else if (service == 4) {
+
+                  output.writeUTF("SERVER>\n\t " + mult(input1, input2));
+                  
+               } else if (service == 5) {
+
+                  output.writeUTF("SERVER>\n\t " + qout(input1, input2));
+                  
+               } else {
+                  output.writeUTF("SERVER>\n\t " + "invalid choice");
+
+               }
+
+               
+               service = input.readInt();
+               input1 = input.readInt();
+               input2 = input.readInt();
+
+            } while (service != 0);
+
+            output.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
+               + "\nGoodbye!");
+
+            server.close();
+            
+            
+            
+         } catch (SocketTimeoutException s) {
+
+            System.out.println("Socket timed out \nGoodbye!");
+            break;
+
+         } catch (IOException e) {
+
+            e.printStackTrace();
+            break;
+
+         }
+      }
+   }
+   
+
+   private String menu() {
+      return "\tMath Server\n***************************\nchoose a number for the coresponding service\nthen send response in this format\n\n\tservice: (int)\n\tinput1: (int)\n\tinput2: (int)\n\n 0. Quit\n 1. Print this help message\n 2. Addition\n 3. Subtraction\n 4. Multiplication\n 5. Division";
+   }
+
+   private int add(int a, int b) {
+      return a + b;
+   }
+
+   private int diff(int a, int b) {
+      return a - b;
+   }
+
+   private int mult(int a, int b) {
+      return a*b;
+   }
+
+   private double qout(int a, int b) {
+      return (double)a/b;
+   }
+}
